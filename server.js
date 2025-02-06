cdb42741dc6ee9565d1f1db3f7eab819053d9648
Muhammad Usama Safdar <usamasafdar540@gmail.com>
2025-02-06 19:33:08 +0500

    Initial Commit

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const morgan = require("morgan");
+const dotEnv = require("dotenv").config();
+const cors = require("cors");
+// const errorHandler = require("./middlewares/errorHandler/errors");
+// const dbConnect = require("./config/dbconnection");
+// const route = require("./routes/routes");
+const app = express();
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+// const bodyParser = require("body-parser");
+// const route = require("./routes/route");
+const port = process.env.PORT || 5001;
+// require("./config/db");
+app.use(express.json());
+app.use(morgan("combined"));
+dbConnect();
+app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use("/", route);
+app.use(errorHandler);
+// app.use("/", (req, res) => {
+//   res.send("Hello From The Serve Side");
+// });
+app.listen(port, () => {
+  console.log(`listening to the ${port}`);
+});
